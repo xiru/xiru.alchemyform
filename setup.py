c35c5c15a50f18b6fd4ee3f75f721307e70c67a7
Fabiano Weimar dos Santos <xiru@xiru.org>
2012-09-18 13:54:22 -0300

    Initial import of the code generated by ZopeSkel.

---
+++ setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup, find_packages
+import os
+
+version = '1.0'
+
+setup(name='xiru.alchemyform',
+      version=version,
+      description="A CRUD implementation using Plone, SQLAlchemy and z3c.form",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from
+      # http://pypi.python.org/pypi?:action=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        ],
+      keywords='plone sqlalchemy z3c.form',
+      author='Fabiano Weimar dos Santos',
+      author_email='xiru@xiru.org',
+      url='https://github.com/xiru/xiru.alchemyform',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['xiru'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      setup_requires=["PasteScript"],
+      paster_plugins=["ZopeSkel"],
+      )
